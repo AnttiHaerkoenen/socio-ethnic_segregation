59a965f33901318e74aaea0270e16868b95c4439
antth <antth@hotmail.fi>
2022-08-15 13:32:34 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Socio-ethnic segregation in Vyborg in 1880',
+    author='Antti Härkönen',
+    license='BSD-3',
+)
